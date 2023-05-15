b7c25ef139ddf6da7639b73eb5a46026ec66c411
Douglas Sant Anna <douglbb1@gmail.com>
2023-05-14 23:42:09 -0300

    gerando proposta em pdf

--- app/src/app/proposals/view-pdf/view-pdf.component.ts
+++ app/src/app/proposals/view-pdf/view-pdf.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Proposal } from '../proposal';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-
+import { jsPDF } from "jspdf";
+import { ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-view-pdf',
   templateUrl: './view-pdf.component.html',
@@ -9,10 +10,20 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class ViewPdfComponent implements OnInit {
   displayedColumns: string[] = ['name', 'quantity'];
+  @ViewChild('content') content!: ElementRef;
   constructor(
     @Inject(MAT_DIALOG_DATA) public proposal: Proposal
   ) { }
+
   ngOnInit(): void {
     console.log(this.proposal)
+  }
+  printPDF() {
+    const doc = new jsPDF('p', 'pt', 'a4');
+    doc.html(this.content.nativeElement, {
+      callback: (doc) => {
+        doc.save();
+      }
+    });
   }
 }
