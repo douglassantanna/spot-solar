9ced4a1aace9cef4ed07f212fcc0ca1945bbd931
Douglas Sant Anna <douglbb1@gmail.com>
2023-05-20 11:36:27 -0300

    botao de ver pdf funcionando. adicao de total preco produtos e total preco mao de obra no pdf

--- app/src/app/proposals/view/view.component.ts
+++ app/src/app/proposals/view/view.component.ts
@@ -3,6 +3,8 @@ import { SpotSolarService } from 'src/app/services/spot-solar.service';
 
 import { Proposal } from '../../interfaces/proposal';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { ViewPdfComponent } from '../view-pdf/view-pdf.component';
 
 @Component({
   selector: 'app-view',
@@ -15,6 +17,7 @@ export class ViewComponent {
   signalDataSource = signal<any[]>([]);
   private spotService = inject(SpotSolarService);
   private router = inject(Router);
+  private dialog = inject(MatDialog);
 
   constructor() {
     this.getProposals();
@@ -29,5 +32,12 @@ export class ViewComponent {
   }
   editProposal(proposal: Proposal) {
     this.router.navigate(['/edit', proposal.id]);
+  }
+  viewPDF(proposal: Proposal) {
+    const dialogRef = this.dialog.open(ViewPdfComponent, {
+      data: proposal,
+      width: '610px',
+      height: '90%',
+    })
   }
 }
