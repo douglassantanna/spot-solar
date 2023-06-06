305fb8329b2f06ab66dcca3ea2f890647599a6b8
Douglas Sant Anna <douglbb1@gmail.com>
2023-06-05 23:13:49 -0300

    resolvendo cors

---
+++ api/Startup.cs
@@ -0,0 +1,18 @@
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(api.Startup))]
+namespace api;
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+
+    }
+    public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+    {
+
+        base.ConfigureAppConfiguration(builder);
+    }
+
+}
